e15ba1a1324101b01a113ee737b550a05a775abf
GodwinKadima <gs939@live.mdx.ac.uk>
2026-05-05 09:47:26 +0300

    Fix password length validation in registration process

--- app.py
+++ app.py
@@ -24,8 +24,10 @@ while True:
     # Check Password Lenght
      elif len(password) < 6 : 
         print('Too Short')
-     elif len(password) > 10 :
+     elif len(password) >= 10 :
+        users[username] = password
         print('Execellent')
+
      else :
         users[username] = password
         print('Saved')
