3ce97bb7e038e1267dfd63de17dc428c8b6336e4
Godwin <135100112+godwink12@users.noreply.github.com>
2026-05-05 10:14:18 +0300

    Refactor password validation logic in registration process

--- app.py
+++ app.py
@@ -1,47 +1,46 @@
+import string
+
 # Login System 
 
 print('Welcome to our  Website Glad to Have You here Login For more More Access!!')
 
-
 users = {}
-
-
-
 
 while True:
   user_choice = input("Do you Want to 'register' , 'login' or 'Exit' ")
 
   if user_choice == 'Exit':
-     print('Thank you!')
-     break
-  
-  # Register Section
+    print('Thank you!')
+    break
   elif user_choice == 'register':
-     username = input("Enter a username: ")
-     password = input('Enter Password: ')
-     if username in users : # Check if User already exist in the dictionary
-        print('username Already existed') 
-    # Check Password Lenght
-     elif len(password) < 6 : 
-        print('Too Short')
-     elif len(password) >= 10 :
-        users[username] = password
-        print('Execellent')
-     else :
-        users[username] = password
+    username = input("Enter a username: ")
+    password = input('Enter Password: ')
+    if username in users:  # Check if user already exists in the dictionary
+      print('username Already existed')
+    elif len(password) < 6:
+      print('Too Weak')
+    elif not any(char.isupper() for char in password):
+      print('Needs a Capital Letter')
+    elif not any(char.isdigit() for char in password):
+      print('Needs a Number')
+    elif not any(char in string.punctuation for char in password):
+      print('Needs a symbol')
+    else:
+      users[username] = password
+      if len(password) >= 10:
+        print('Excellent')
+      else:
         print('Saved')
-
-    # Login Section
   elif user_choice == 'login':
-     username = input("Enter a username: ")
-     passwordL = input('Enter Password: ')
-     # Check User input Exist 
-     if username in users and passwordL == users[username]:
-        print('Welcome')
-     elif username in users and passwordL != users[username]:
-        print('Wrong Password')
-     else :
-        print('USER NOT FOUND!!')
+    username = input("Enter a username: ")
+    passwordL = input('Enter Password: ')
+    # Check User input Exist 
+    if username in users and passwordL == users[username]:
+      print('Welcome')
+    elif username in users and passwordL != users[username]:
+      print('Wrong Password')
+    else :
+      print('USER NOT FOUND!!')
   else:
     print("Invalid choice, try again.")
  
