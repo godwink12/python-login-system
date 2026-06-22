4fbfbb2268ecc5586123ae4a4189e04a45325ce4
godwink12 <godwinkadima1@gmail.com>
2026-06-22 06:27:26 +0300

    Add login, signup, and dashboard templates with SVG assets
    - Created Login.html for user authentication with form fields for username, email, and password.
    - Developed Signup.html for new user registration, including form fields for username and email.
    - Implemented dashboard.html to display user information, task summary, and checklist functionality.
    - Added undraw_all-checked_d3u6.svg as a visual asset for the application.

--- app.py
+++ app.py
@@ -1,54 +1,41 @@
-import string
+from flask import Flask, render_template,request,redirect,url_for, session
+from database import create_table, save_user, find_user,user_exists
+from dotenv import load_dotenv
+import os
 import bcrypt
-from database import create_table, save_user, find_user, user_exists
-
-# Login System 
-
-print('Welcome to our  Website Glad to Have You here Login For more More Access!!')
 
 
-
+app = Flask(__name__)
+load_dotenv()
+app.secret_key = os.getenv('SECRET_KEY')
 create_table()
 
-while True:
-  user_choice = input("Do you Want to 'register' , 'login' or 'Exit' ")
 
-  if user_choice == 'Exit':
-    print('Thank you!')
-    break
-  elif user_choice == 'register':
-    username = input("Enter a username: ")
-    password = input('Enter Password: ')
-    if user_exists(username):  # Check if user already exists in the dictionary
-      print('username Already existed')
-    elif len(password) < 6:
-      print('Too Weak')
-    elif not any(char.isupper() for char in password):
-      print('Needs a Capital Letter')
-    elif not any(char.isdigit() for char in password):
-      print('Needs a Number')
-    elif not any(char in string.punctuation for char in password):
-      print('Needs a symbol')
-    else:
-      password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-      save_user(username,password_hash)
-      if len(password) >= 10:
-        print('Excellent')
-      else:
-        print('Saved')
-  elif user_choice == 'login':
-    username = input("Enter a username: ")
-    passwordL = input('Enter Password: ')
-    # Check User input Exist 
-    if user_exists(username) and bcrypt.checkpw(passwordL.encode('utf-8'), find_user(username)[1]):
-      print('Welcome')
-    elif user_exists(username) and not bcrypt.checkpw(passwordL.encode('utf-8'), find_user(username)[1]):
-      print('Wrong Password')
-    else :
-      print('USER NOT FOUND!!')
-  else:
-    print("Invalid choice, try again.")
+@app.route('/')
+def home():
+    return render_template('Login.html')
 
+@app.route('/login', methods= ['GET', 'POST'])
+def login():
     
-     
+    if request.method == 'POST':
+        username = request.form["username"]
+        passwordL = request.form['password' ]
+    # Check User input Exist 
+        if user_exists(username):
+            if bcrypt.checkpw(passwordL.encode('utf-8'), find_user(username)[1]):
+                session['username'] = username
+                return redirect(url_for('dashboard'))
+            else:
+                return render_template('Login.html',error = 'Wrong Password')
+        else :
+            return render_template('Login.html', error='User not found')
 
+    return render_template('Login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
