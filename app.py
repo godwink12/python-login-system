2dae0dc2b5d10e0b6150e3b0b3e31ac169608f3d
godwink12 <godwinkadima1@gmail.com>
2026-06-22 07:32:17 +0300

    Implement user registration with password strength validation and update login/signup templates

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from database import create_table, save_user, find_user,user_exists
 from dotenv import load_dotenv
 import os
 import bcrypt
+import string
 
 
 app = Flask(__name__)
@@ -20,10 +21,13 @@ def login():
     
     if request.method == 'POST':
         username = request.form["username"]
-        passwordL = request.form['password' ]
+        password = request.form['password' ]
     # Check User input Exist 
         if user_exists(username):
-            if bcrypt.checkpw(passwordL.encode('utf-8'), find_user(username)[1]):
+            stored_hash = find_user(username)[1]
+            if isinstance(stored_hash, str):
+                stored_hash = stored_hash.encode('utf-8')
+            if bcrypt.checkpw(password.encode('utf-8'), stored_hash):
                 session['username'] = username
                 return redirect(url_for('dashboard'))
             else:
@@ -33,9 +37,34 @@ def login():
 
     return render_template('Login.html')
 
+@app.route('/register', methods= ['GET', 'POST'])
+def register():
+
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form['password' ]
+
+        if user_exists(username):
+            return render_template('Signup.html' , error='username Already existed')
+        elif len(password) < 6:
+            return render_template('Signup.html', error='Too Weak')
+        elif not any(char.isupper() for char in password):
+            return render_template('Signup.html', error='Needs a Capital Letter')
+        elif not any(char.isdigit() for char in password):
+            return render_template('Signup.html', error='Needs a Number')
+        elif not any(char in string.punctuation for char in password):
+            return render_template('Signup.html', error='Needs a symbol')
+        else:
+            password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            save_user(username,password_hash)
+            return redirect(url_for('login'))
+    
+    return render_template('Signup.html')
+
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
